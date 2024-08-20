0cb40ff557d8fc6107d0c75506547125b619f0fd
eKuuuuu <erik.roigas@metropolia.fi>
2024-08-20 14:18:50 +0300

    3 tehtävää

---
+++ tehtavat/tehtava2.py
@@ -0,0 +1,11 @@
+def nimi():
+    nimi = input("Anna nimesi: ")
+    print("Terve, " +nimi + "!")
+
+def ympyra():
+    sade = int(input("Mikä on ympyrän säde?"))
+    pintaala = 3.14 * (sade*sade)
+    print("Ympyrän pinta-ala on ", pintaala)
+
+ympyra()
+
